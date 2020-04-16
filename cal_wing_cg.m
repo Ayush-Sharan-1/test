b52bf69c0033c380b8bfcd06bc6f8157f012fca2
Ayush-Sharan-1 <ayush00414@gmaiil.com>
2020-04-16 17:01:01 +0530

    first version of test

---
+++ cal_wing_cg.m
@@ -0,0 +1,12 @@
+function [ x,y ] = cal_wing_cg(Croot,Ctip,LEangle,span,DFC )
+%Calculates CG of the Wing wrt the LEMAC in feet
+%   Detailed explanation goes here
+LEangle=degtorad(LEangle);
+y=(0.35*span)/2;
+c=(2*y*(Ctip-Croot))/span+Croot
+Xfs=0.25*c; %front spar locations, VARIABLE
+Xrs=0.6*c; % rear spar location, VARIABLE
+x=0.7*(Xrs-Xfs)+y*tan(LEangle)-DFC*tan(LEangle);
+
+end
+
